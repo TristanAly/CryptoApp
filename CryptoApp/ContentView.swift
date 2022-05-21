679eac1be8c429569473fa837c9803440594e956
AlyTristan <tristan.aly@gmail.com>
2022-05-21 13:01:02 +0200

    Initial Commit

---
+++ CryptoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoApp
+//
+//  Created by Tristan Aly on 21/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Tristan Aly on 21/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
